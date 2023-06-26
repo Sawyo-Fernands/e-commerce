4e44bdfae0db31c0f951df56d099c352e68231ef
Sawyo Fernands <78683353+Sawyo-Fernands@users.noreply.github.com>
2023-06-26 19:48:45 -0300

    style[Catalogo] : Criando estrutura inicial do header da página

---
+++ src/pages/Catalogo/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { Header } from '@/components/Header'
+import styles from './style.module.scss'
+
+
+export function Catalogo(){
+
+    return(
+        <>
+        <Header />
+        </>
+    )
+}
