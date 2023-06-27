99975dd710615ae8cd69798ac3664a1bed4f0d6f
Sawyo Fernands <78683353+Sawyo-Fernands@users.noreply.github.com>
2023-06-26 21:13:31 -0300

    style [catalogo] : Adicionando fonte Saira

--- src/app/components/Product/index.tsx
+++ src/app/components/Product/index.tsx
@@ -1,29 +1,26 @@
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
-interface ProductProps{
-    imgSrc:string;
-    name:string;
-    price:number
+interface ProductProps {
+  imgSrc: string;
+  name: string;
+  price: number;
 }
 
-export function Product({imgSrc,name,price}:ProductProps){
-
-    return(
-        <div className={styles.containerCard}>
-            <img src={imgSrc} alt={name} />
-           <div className={styles.containerInfos}>
-            <div className={styles.containerText}>
-             <span className={styles.name}>{name}</span>
-            </div>
-           
-            <div className={styles.line}></div>
-
-            <div className={styles.containerText}>
-            <span className={styles.price}>{price}</span>
-            </div>
-           </div>
-
+export function Product({ imgSrc, name, price }: ProductProps) {
+  return (
+    <div className={styles.containerCard}>
+      <img src={imgSrc} alt={name} />
+      <div className={styles.containerInfos}>
+        <div className={styles.containerText}>
+          <span className={styles.name}>{name}</span>
         </div>
-        
-    )
+
+        <div className={styles.line}></div>
+
+        <div className={styles.containerText}>
+          <span className={styles.price}>{price}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
