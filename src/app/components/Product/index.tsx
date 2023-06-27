6eaa4304c399e929333b53093f2c093777bf0f38
Sawyo Fernands <78683353+Sawyo-Fernands@users.noreply.github.com>
2023-06-26 21:07:33 -0300

    feat [catalogo] : hook listagem de produtos. style [catalogo] : Iniciando estrutura do componente de produto.

--- src/app/components/Product/index.tsx
+++ src/app/components/Product/index.tsx
@@ -1,11 +1,29 @@
 import styles from './styles.module.scss'
 
+interface ProductProps{
+    imgSrc:string;
+    name:string;
+    price:number
+}
 
-export function Product(){
+export function Product({imgSrc,name,price}:ProductProps){
 
     return(
-        <div>
+        <div className={styles.containerCard}>
+            <img src={imgSrc} alt={name} />
+           <div className={styles.containerInfos}>
+            <div className={styles.containerText}>
+             <span className={styles.name}>{name}</span>
+            </div>
+           
+            <div className={styles.line}></div>
+
+            <div className={styles.containerText}>
+            <span className={styles.price}>{price}</span>
+            </div>
+           </div>
 
         </div>
+        
     )
 }
