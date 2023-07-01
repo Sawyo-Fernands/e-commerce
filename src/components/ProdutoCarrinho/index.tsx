e8ba92c9bd8b2381967874bb2dc1a280296358fe
Sawyo Fernands <78683353+Sawyo-Fernands@users.noreply.github.com>
2023-06-30 21:56:22 -0300

    feat[carrinhoCompras] : Somando valore de itens do carrinho e listando.

--- src/components/ProdutoCarrinho/index.tsx
+++ src/components/ProdutoCarrinho/index.tsx
@@ -1,14 +1,31 @@
 import { formatPrice } from '@/utils/formatCurrency';
 import styles from './styles.module.scss'
 import {FaTrashAlt} from 'react-icons/fa'
+import { useContext, useState } from 'react';
+import { ProductCarrinhoContext } from '@/context/useProductsCarrinho';
 interface ProdutoCarrinhoProps{
     img:string;
     description:string;
     name:string;
     price:number;
+    id:string;
 }
 
-export function ProdutoCarrinho({img,description,name,price}:ProdutoCarrinhoProps){
+export function ProdutoCarrinho({img,description,name,price,id}:ProdutoCarrinhoProps){
+
+    const { products,setProducts } = useContext(ProductCarrinhoContext)
+
+    const [quantidade,setQuantidade] = useState(1)
+
+    function handleRemoveProduct(){
+        setProducts(products.filter((product) => product.id !== id))
+    }
+
+    const TextoComLimite = ({ texto, limite }:{texto:string,limite:number}) => {
+        const textoTruncado = texto.slice(0, limite) + '...';
+      
+        return textoTruncado;
+    };
 
     return(
         <div className={styles.containerProduto}>
@@ -19,17 +36,17 @@ export function ProdutoCarrinho({img,description,name,price}:ProdutoCarrinhoProp
                 <div style={{width:"95%",margin:"auto"}}>
                 <div className={styles.containerHeaderproduct}>
                 <h4>{name}</h4>
-                <FaTrashAlt size={21} color='#CD1818' />
+                <FaTrashAlt size={21} color='#CD1818' onClick={handleRemoveProduct} style={{cursor:'pointer'}} />
                 </div>
                 <div>
                 <span className={styles.description}>
-                    {description}
+                    {TextoComLimite({texto:description,limite:250})}
                 </span>
                 </div>
                 <div className={styles.containerSelect}>
-                <select name="select" className={styles.select}>
-                <option value="1">1</option>
-                <option value="2" selected>2</option>
+                <select name="select" onChange={(e)=>setQuantidade(Number(e.target.value))} className={styles.select}>
+                <option value="1" selected>1</option>
+                <option value="2" >2</option>
                 <option value="3">3</option>
                 <option value="3">4</option>
                 <option value="3">5</option>
